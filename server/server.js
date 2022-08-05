0dd0d98af44f2ae0db5655cf22de7451f5411141
Anil <meghanilva@live.in>
2022-08-05 16:08:16 +0530

    heroku dep

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path')   ; 
+
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('build'));
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname,'build','index.html'));
+    })
+}
+
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=> console.log("listening"));
+
+//heroku login
